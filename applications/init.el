9b5d759e0a193c2cbab18a04cc7540087f1df8dc
Jean Gregory Verret <gregory.verret@gmail.com>
2020-11-26 14:39:15 -0500

    divide configuration file

---
+++ applications/init.el
@@ -0,0 +1,419 @@
+(defconst medivhok:org-directory
+  (file-name-as-directory "~/org")
+  "The root directory of the org ecosystem.")
+
+(defconst medivhok:agenda-directory
+  (file-name-as-directory
+   (expand-file-name "agenda" medivhok:org-directory))
+  "The directory of my agenda files.")
+
+(defconst medivhok:pdf-directory
+  (file-name-as-directory
+   (expand-file-name "readings" medivhok:org-directory))
+  "The directory of my pdf files.")
+
+(defconst medivhok:roam-directory
+  (file-name-as-directory
+   (expand-file-name "roam" medivhok:org-directory))
+  "The directory of my roam files.")
+
+(defconst medivhok:bibtex-file
+  (expand-file-name "zotero.bib" medivhok:pdf-directory)
+  "My default bibliography file.")
+
+(use-package org
+  :init
+  (setq org-directory medivhok:org-directory)
+
+  :hook
+  (org-mode . org-indent-mode)
+  (org-mode . variable-pitch-mode)
+  (org-mode . visual-line-mode)
+
+  :custom-face
+  (org-block ((t (:inherit fixed-pitch))))
+  (org-document-title ((t (:height 1.5 :weight light))))
+  (org-level-1 ((t (:inherit outline-1 :height 1.3 :weight light))))
+  (org-level-2 ((t (:inherit outline-2 :height 1.2 :weight light))))
+  (org-level-3 ((t (:inherit outline-3 :height 1.1 :weight light))))
+  (org-link ((t (:inherit link :underline nil :weight regular))))
+  (org-meta-line ((t (:inherit fixed-pitch))))
+  (org-table ((t (:inherit org-default :family "Roboto Mono"))))
+  (org-table-header ((t (:inherit org-table :family "Roboto Mono"))))
+
+  :general
+  (medivhok:local-mode-menu 'normal
+    org-mode-map
+    nil '(:ignore t :which-key "org")
+    "e" 'org-export-dispatch
+    "t" '(:ignore t :which-key "toggle")
+    "tl" 'org-toggle-link-display)
+
+  :custom
+  (org-fontify-quote-and-verse-blocks t)
+
+  :config
+  (setq org-agenda-files (list medivhok:agenda-directory)
+        org-catch-invisible-edits 'show
+        org-cycle-separator-lines 2
+        org-directory medivhok:app-directory
+        org-edit-src-content-indentation 0
+        org-ellipsis " ▼"
+        org-hide-block-startup nil
+        org-hide-emphasis-markers t
+        org-log-done 'time
+        org-log-into-drawer t
+        org-outline-path-complete-in-steps nil
+        org-return-follows-link t
+        org-src-fontify-natively t
+        org-src-preserve-indentation nil
+        org-src-tab-acts-natively t
+        org-src-window-setup 'current-window
+        org-startup-folded t)
+
+  (setq org-format-latex-options
+        (plist-put org-format-latex-options :scale 4.0))
+  (org-babel-do-load-languages
+   'org-babel-load-languages
+   '((emacs-lisp . t)
+     (haskell . t)
+     (ledger . t)
+     (R . t)))
+
+  (add-hook 'org-src-mode-hook
+            (lambda ()
+              (when (eq major-mode 'emacs-lisp-mode)
+                (setq flycheck-disabled-checkers '(emacs-lisp-checkdoc)))))
+
+  ;; Replace list hyphen with dot.
+  (require 'org-indent)
+  (font-lock-add-keywords 'org-mode
+                          '(("^ *\\([-]\\) "
+                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•")))))))
+
+(use-package ob-core
+  :defer t
+  :straight org
+  :custom
+  (org-confirm-babel-evaluate nil))
+
+(use-package ob-tangle
+  :defer t
+
+  :straight org
+
+  :preface
+  (defun medivhok/tangle-on-save ()
+    "Disable confirmation before tangling."
+    (let ((org-confirm-babel-evaluate nil))
+      (org-babel-tangle)))
+
+  :hook
+  (org-mode . (lambda ()
+                (add-hook 'after-save-hook
+        	                #'medivhok/tangle-on-save
+                          'run-at-end
+                          'only-in-org-mode))))
+
+(use-package org-superstar
+  :after org
+
+  :hook
+  (org-mode . org-superstar-mode)
+
+  :custom
+  (org-superstar-remove-leading-stars t)
+  (org-superstar-headline-bullets-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(use-package org-make-toc
+  :hook
+  (org-mode . org-make-toc-mode))
+
+(use-package ox-odt
+  :after org
+
+  :straight org
+
+  :custom
+  (org-odt-convert-process "unoconv")
+  (org-odt-convert-processes '(("unoconv"
+                                "unoconv -f %f %i")))
+  (org-odt-preferred-output-format "docx")
+  (org-odt-prettify-xml t)
+  (org-odt-content-template-file (expand-file-name
+                                  "OrgOdtContentTemplate.xml"
+                                  (concat straight-base-dir
+                                          "straight/repos/org/etc/styles/")))
+  (org-odt-styles-file (expand-file-name
+                        "OrgOdtStyles.xml"
+                        (concat straight-base-dir
+                                "straight/repos/org/etc/styles/"))))
+
+  ;; :config
+  ;; (org-odt-add-automatic-style "TNOrgTitle"
+  ;;                             '(("style:family" "paragraph")
+  ;;                               ("style:parent-style-name" "OrgTitle")
+  ;;                               ("style:master-page-name" "OrgTitlePage"))))
+
+(use-package evil-org
+  :hook
+  ((org-mode . evil-org-mode)
+   (org-agenda-mode . evil-org-mode)
+   (evil-org-mode . (lambda ()
+	                    (evil-org-set-key-theme))))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
+
+(use-package helm-bibtex
+  :after (helm org)
+  :init
+  (setq bibtex-completion-bibliography medivhok:bibtex-file
+        bibtex-completion-notes-path medivhok:roam-directory
+        bibtex-completion-pdf-field "File"))
+
+(use-package org-ref
+  :after
+  (org helm-bibtex)
+
+  :init
+  (setq org-ref-completion-library 'org-ref-helm-cite
+        org-ref-default-bibliography (list medivhok:bibtex-file)))
+
+(defun medivhok/open-agenda ()
+  "Opens my GTD agenda."
+  (interactive)
+  (org-agenda nil " "))
+
+(use-package org-agenda
+  :defer t
+
+  :after (org)
+
+  :straight org
+
+  :init
+  (defconst medivhok:gtd-file
+    (expand-file-name "gtd.org" medivhok:agenda-directory)
+    "My 'getting things done' agenda file.")
+
+  :custom
+  (org-agenda-window-setup 'current-window)
+  (org-agenda-block-separator nil)
+  (org-agenda-dim-blocked-tasks 'invisible)
+  (org-agenda-skip-deadline-if-done t)
+  (org-agenda-skip-scheduled-if-done t)
+  (org-agenda-start-with-log-mode t)
+  (org-agenda-custom-commands
+   `((" "
+      "GTD Agenda"
+      ((agenda ""
+	             ((org-agenda-span 'week)
+	              (org-deadline-warning-days 14)))
+       (tags-todo "@inbox"
+		              ((org-agenda-overriding-header "Inbox")))
+       (tags-todo "@tâches"
+		              ((org-agenda-overriding-header "Tâches")))
+       (tags-todo "@teluq"
+		              ((org-agenda-overriding-header "Teluq")))
+       (tags-todo "@projets"
+		              ((org-agenda-overriding-header "Projets"))))))))
+
+(use-package org-capture
+  :straight org
+
+  :commands (org-capture)
+  :bind
+  (("<f4>" . (lambda () (interactive) (org-capture nil "i"))))
+
+  :config
+  (setq org-capture-templates
+	`(("i" "inbox" entry
+	   (file+headline ,medivhok:gtd-file "Inbox")
+	   "* TODO [#C] %?\n:PROPERTIES:\n:Effort: 1\n:END:\n")
+
+	  ("e" "email" entry
+	   (file+headline ,medivhok:gtd-file "Emails")
+	   "* TODO [#A] Reply: %a"
+	   :immediate-finish t))))
+
+(use-package org-refile
+  :straight org
+
+  :config
+  (setq org-refile-allow-creating-parent-nodes 'confirm
+	      org-refile-use-outline-path 'file
+	      org-refile-targets '((nil :tag . "@tâches")
+	                     	   (nil :tag . "@cours")
+				   (nil :tag . "@projet")
+				   (nil :tag . "@teluq"))))
+
+(use-package org-roam
+  :after
+  (org helm)
+
+  :straight
+  (org-roam :host github :repo "org-roam/org-roam")
+
+  :hook
+  (after-init . org-roam-mode)
+
+  :commands
+  (org-roam-db-query)
+
+  :general
+  (medivhok:notes-menu
+    :states 'normal
+    "f" 'org-roam-find-file
+    "G" 'org-roam-graph
+    "i" 'org-roam-insert
+    "r" 'org-roam-buffer-toggle-display)
+
+  :init
+  (defconst medivhok:roam-templates-directory
+    (file-name-as-directory
+     (expand-file-name "templates" medivhok:roam-directory))
+    "The slip box cards templates directory.")
+
+  (setq org-roam-completion-system 'helm
+        org-roam-directory medivhok:roam-directory
+        org-roam-file-exclude-regexp "setupfiles\\|templates"
+        org-roam-index-file "index_file.org"
+        org-roam-tag-sources '(prop)
+        org-roam-title-sources '(title alias)
+        org-roam-capture-templates
+        `(("n" "note card" plain
+           (function org-roam--capture-get-point)
+           "%?"
+           :file-name "%<%Y%m%d%H%M%S>-${slug}"
+           :head "#+TITLE: ${title}
+#+CREATED: %T
+#+LAST_MODIFIED: %T
+
+- tags ::"
+           :unnarrowed t))
+
+        org-roam-ref-capture-templates
+        '(("r" "ref" plain (function org-roam-capture--get-point)
+           "%?"
+           :file-name "%<%Y%m%d%H%M%S>-${slug}"
+           :head "#+TITLE: ${title}\n#+ROAM_KEY: ${ref}\n- source :: ${ref}"
+           :unnarrowed t)))
+
+  (setq time-stamp-active t
+        time-stamp-pattern "-10/^#\\+LAST_MODIFIED: <%Y-%02m-%02d %a %02H:%02M>$"
+        time-stamp-format "%Y-%02m-%02d %a %02H:%02M")
+
+  (defun medivhok/sanitize-roam-tags (item &optional item-list)
+    (let ((current-list (or item-list (list))))
+      (if (stringp item)
+          (unless (member item current-list)
+            (push item current-list))
+        (dolist (current-item item)
+          (setq current-list (medivhok/sanitize-roam-tags current-item current-list))))
+      current-list))
+
+  (defun medivhok/get-roam-tags ()
+    "Return a list of the tags in my roam files."
+    (medivhok/sanitize-roam-tags (org-roam-db-query [:select tags :from tags])))
+
+  (defun medivhok/get-roam-tag-alist ()
+    (let ((tag-list (medivhok/get-roam-tags))
+          (tag-alist))
+      (dolist (tag tag-list tag-alist)
+        (push (cons tag nil) tag-alist))))
+
+  (defun medivhok/select-roam-tags ()
+    "Return a list of selected tags."
+    (let ((available-tag-list (medivhok/get-roam-tags))
+          (selected-tag-list)
+          (selected-tag)
+          (done-selection "[done]")
+          (finished nil))
+      (add-to-list 'available-tag-list done-selection)
+      (while (not finished)
+        (let ((selected-tag
+               (completing-read
+                (format "Tags %s: " (or selected-tag-list ""))
+                available-tag-list)))
+          (cond ((or (string= "" selected-tag)
+                     (string= done-selection selected-tag))
+                 (setq finished t))
+                ((not selected-tag)
+                 (message "Return is nil"))
+                (t
+                 (delete selected-tag available-tag-list)
+                 (add-to-list 'selected-tag-list selected-tag t)))))
+      selected-tag-list)))
+
+(use-package org-roam-bibtex
+  :straight
+  (org-roam-bibtex :host github :repo "org-roam/org-roam-bibtex")
+
+  :after
+  (org-roam helm-bibtex org-ref)
+
+  :hook
+  (org-roam-mode . org-roam-bibtex-mode)
+
+  :init
+  (setq org-ref-notes-function 'orb-edit-notes
+        orb-preformat-keywords '(("citekey" . "=key=")
+                                 "title"
+                                 "url"
+                                 "file"
+                                 "author-or-editor"
+                                 "keywords")
+        orb-templates `(("r" "ref" plain (function org-roam-capture--get-point)
+                         ""
+                         :file-name "%<%Y%m%d%H%M%S>-${citekey}"
+                         :head
+                         ,(concat "#+TITLE: ${title}\n"
+                                  "#+ROAM_KEY: ${ref}\n"
+                                  "* Notes\n"
+                                  ":PROPERTIES:\n"
+                                  ":Custom_ID: ${citekey}\n"
+                                  ":URL: ${url}\n"
+                                  ":AUTHOR: ${author-or-editor}\n"
+                                  ":NOTER_DOCUMENT: %(orb-process-file-field \"${citekey}\")\n"
+                                  ":END:\n\n")
+                         :unnarrowed t)
+
+                        ("w" "webpage" plain (function org-roam-capture--get-point)
+                         ""
+                         :file-name "%<%Y%m%d%H%M%S>-${slug}"
+                         :head
+                         ,(concat "#+TITLE: ${title}\n"
+                                  "#+ROAM_KEY: ${url}\n\n"
+                                  "* Notes\n"
+                                  ":PROPERTIES:\n"
+                                  ":Custom_ID: ${citekey}\n"
+                                  ":URL: ${url}\n"
+                                  ":END:\n\n")
+                         :unnarrowed t))))
+
+(use-package ledger-mode
+  :defer t)
+
+(use-package flycheck-ledger
+  :after flycheck)
+
+(use-package evil-ledger
+  :hook
+  (ledger-mode . evil-ledger-mode)
+
+  :general
+  (medivhok:local-mode-menu 'normal
+    "s" 'evil-ledger-sort))
+
+(use-package ess
+  :defer t)
+
+(use-package ess-R-data-view
+  :defer t)
+
+(use-package polymode
+  :defer t)
+
+(use-package poly-R
+  :defer t)
---
+++ ides/init.el
@@ -0,0 +1,307 @@
+(use-package company
+  :hook
+  (after-init . global-company-mode)
+
+  :config
+  (setq company-box-icons-all-the-icons
+        (let ((all-the-icons-scale-factor 0.8))
+          `((Unknown       . ,(all-the-icons-material "find_in_page"             :face 'all-the-icons-purple))
+            (Text          . ,(all-the-icons-material "text_fields"              :face 'all-the-icons-green))
+            (Method        . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+            (Function      . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+            (Constructor   . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+            (Field         . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+            (Variable      . ,(all-the-icons-material "adjust"                   :face 'all-the-icons-blue))
+            (Class         . ,(all-the-icons-material "class"                    :face 'all-the-icons-red))
+            (Interface     . ,(all-the-icons-material "settings_input_component" :face 'all-the-icons-red))
+            (Module        . ,(all-the-icons-material "view_module"              :face 'all-the-icons-red))
+            (Property      . ,(all-the-icons-material "settings"                 :face 'all-the-icons-red))
+            (Unit          . ,(all-the-icons-material "straighten"               :face 'all-the-icons-red))
+            (Value         . ,(all-the-icons-material "filter_1"                 :face 'all-the-icons-red))
+            (Enum          . ,(all-the-icons-material "plus_one"                 :face 'all-the-icons-red))
+            (Keyword       . ,(all-the-icons-material "filter_center_focus"      :face 'all-the-icons-red))
+            (Snippet       . ,(all-the-icons-material "short_text"               :face 'all-the-icons-red))
+            (Color         . ,(all-the-icons-material "color_lens"               :face 'all-the-icons-red))
+            (File          . ,(all-the-icons-material "insert_drive_file"        :face 'all-the-icons-red))
+            (Reference     . ,(all-the-icons-material "collections_bookmark"     :face 'all-the-icons-red))
+            (Folder        . ,(all-the-icons-material "folder"                   :face 'all-the-icons-red))
+            (EnumMember    . ,(all-the-icons-material "people"                   :face 'all-the-icons-red))
+            (Constant      . ,(all-the-icons-material "pause_circle_filled"      :face 'all-the-icons-red))
+            (Struct        . ,(all-the-icons-material "streetview"               :face 'all-the-icons-red))
+            (Event         . ,(all-the-icons-material "event"                    :face 'all-the-icons-red))
+            (Operator      . ,(all-the-icons-material "control_point"            :face 'all-the-icons-red))
+            (TypeParameter . ,(all-the-icons-material "class"                    :face 'all-the-icons-red))
+            (Template      . ,(all-the-icons-material "short_text"               :face 'all-the-icons-green))
+            (ElispFunction . ,(all-the-icons-material "functions"                :face 'all-the-icons-red))
+            (ElispVariable . ,(all-the-icons-material "check_circle"             :face 'all-the-icons-blue))
+            (ElispFeature  . ,(all-the-icons-material "stars"                    :face 'all-the-icons-orange))
+            (ElispFace     . ,(all-the-icons-material "format_paint"             :face 'all-the-icons-pink)))))
+
+  (defun medivhok/company-backend-with-yas (backends)
+    "Add :with company-yasnippet to company BACKENDS.
+Taken from https://github.com/syl20bnr/spacemacs/pull/179."
+    (if (and (listp backends) (memq 'company-yasnippet backends))
+	      backends
+	    (append (if (consp backends)
+		              backends
+		            (list backends))
+		          '(:with company-yasnippet))))
+
+  ;; add yasnippet to all backends
+  (setq company-backends
+        (mapcar #'medivhok/company-backend-with-yas company-backends)))
+
+(use-package company-dict
+  :after company)
+
+(use-package company-box
+  :after company
+
+  :hook
+  (company-mode . company-box-mode))
+
+(use-package helm
+  :config
+  (require 'helm-config))
+
+(use-package helm-lsp
+  :after
+  (helm lsp-mode))
+
+(use-package lsp-mode
+  :defer t
+  :after which-key
+  :hook
+  (lsp-mode . lsp-enable-which-key-integration))
+
+(use-package pdf-tools
+  :defer t
+  :mode ("\\.[pP][dD][fF]\\'" . pdf-view-mode)
+  :magic ("%PDF" . pdf-view-mode)
+  :config
+  (pdf-loader-install)
+  (evil-collection-init 'pdf)
+  (evil-collection-pdf-setup))
+
+(setq-default tab-width 2)
+(setq-default evil-shift-with tab-width)
+(global-auto-revert-mode t)
+
+(setq-default indent-tabs-mode nil)
+
+(use-package hideshow)
+
+(use-package smartparens
+  :hook
+  (prog-mode . smartparens-mode)
+  (prog-mode . smartparens-strict-mode)
+
+  :config
+  (require 'smartparens-config))
+
+(use-package evil-nerd-commenter
+  :bind
+  ("M-/" . evilnc-comment-or-uncomment-lines))
+
+(use-package ws-butler
+  :hook
+  ((text-mode . ws-butler-mode)
+   (prog-mode . ws-butler-mode)))
+
+(use-package parinfer
+  :hook ((clojure-mode . parinfer-mode)
+         (emacs-lisp-mode . parinfer-mode)
+         (common-lisp-mode . parinfer-mode)
+         (scheme-mode . parinfer-mode)
+         (lisp-mode . parinfer-mode))
+  :config
+  (setq parinfer-extensions
+      '(defaults       ; should be included.
+        pretty-parens  ; different paren styles for different modes.
+        evil           ; If you use Evil.
+        smart-tab      ; C-b & C-f jump positions and smart shift with tab & S-tab.
+        smart-yank)))  ; Yank behavior depend on mode.
+
+(use-package rainbow-delimiters
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
+
+(use-package yasnippet
+  :custom
+  (yas-snippet-dirs
+     (list
+      (file-name-as-directory
+       (expand-file-name "snippets"
+                         (file-name-directory user-init-file)))))
+  :general
+  :config
+  (yas-global-mode 1)
+  )
+
+(use-package magit
+  :commands
+  (magit-status magit-get-current-branch)
+
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-execpt-diff-v1))
+
+(use-package evil-magit
+  :after magit)
+
+(use-package git-gutter
+  :hook
+  ((text-mode . git-gutter-mode)
+   (prog-mode . git-gutter-mode))
+
+  :config
+  (setq git-gutter:update-interval 2))
+
+(use-package git-link
+  :commands git-link
+
+  :config
+  (setq git-link-open-in-browser t))
+
+(use-package magit-todos
+  :after magit)
+
+(use-package projectile
+  :config
+  (projectile-mode))
+
+(use-package counsel-projectile
+  :after projectile)
+
+(use-package flycheck
+  :config (global-flycheck-mode))
+
+(use-package css-mode
+  :defer t
+
+  :after
+  (flycheck)
+
+  :init
+  (setq flycheck-css-csslint-executable "~/.yarn/bin/csslint"))
+
+(use-package cask)
+
+(use-package cask-mode
+  :defer t)
+
+(use-package buttercup
+  :defer t)
+
+(use-package haskell-mode
+
+:general
+
+:init
+
+:config
+
+)
+
+(use-package dante
+
+:general
+
+:init
+
+:config
+
+)
+
+(use-package attrap
+
+:general
+
+:init
+
+:config
+
+)
+
+(use-package json-mode
+  :custom
+  (json-reformat:indent-width 2)
+
+  :general
+  (medivhok/local-leader-def 'normal
+    json-mode-map
+    nil '(:ignore t :which-key "json")
+    "d" 'json-decrement-number-at-point
+    "f" 'json-mode-beautify
+    "i" 'json-increment-number-at-point
+    "k" 'json-nullify-sexp
+    "p" 'json-mode-show-path
+    "P" 'json-mode-kill-path
+    "t" 'json-toggle-boolean))
+
+(use-package auctex
+  :defer t
+  :custom
+  (TeX-engine 'luatex))
+
+(use-package company-auctex
+  :defer t
+  :after company)
+
+(use-package company-reftex
+  :defer t
+  :after company)
+
+(use-package company-math
+  :defer t
+  :after company)
+
+(use-package ox-latex
+  :defer t
+  :after org
+  :straight org
+  :config
+  (setq org-latex-caption-above nil
+        org-latex-compiler "lualatex"
+        org-latex-listings 'minted
+        org-latex-minted-options
+        '(("frame" "lines")
+          ("framesep" "2mm")
+          ("baselinestretch" "1.2")
+          ("style" "pastie"))
+        org-latex-packages-alist
+        '(("" "fontspec" t ("lualatex"))
+          ("AUTO" "babel" t ("pdflatex" "lualatex"))
+          ("" "booktabs" t)
+          ("" "fancyhdr" t)
+          ("" "minted")
+          ("" "xcolor"))
+        org-latex-pdf-process
+        '("%latex -shell-escape -interaction nonstopmode -output-directory %o %f"
+          "%latex -shell-escape -interaction nonstopmode -output-directory %o %f"
+          "%latex -shell-escape -interaction nonstopmode -output-directory %o %f"))
+  (add-to-list 'org-latex-minted-langs '(R "r")))
+
+(use-package yaml-mode
+  :mode "\\.ya?ml\\'")
+
+(use-package nxml-mode
+  :straight nxml
+
+  :hook
+  (nxml-mode . hs-minor-mode)
+
+  :general
+  (normal nxml-mode-map "TAB" 'hs-toggle-hiding)
+  (medivhok/local-leader-def 'normal
+    nxml-mode-map
+    nil '(:ignore t :which-key "xml")
+    "t" 'hs-toggle-hiding)
+
+  :config
+  (add-to-list 'hs-special-modes-alist
+               '(nxml-mode
+                "<!--\\|<[^/>]*[^/]>"
+                "-->\\|</[^/>]*[^/]>"
+                "<!--"
+                nxml-forward-element
+                nil)))
