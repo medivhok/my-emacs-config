1a0c3fd9812a9aec938eacb5b845b21d43ff197d
Jean Gregory Verret <gregory.verret@gmail.com>
2020-12-03 14:51:40 -0500

    main init.el refactoring

--- init.el
+++ init.el
@@ -5,23 +5,23 @@
 (setq user-full-name "Jean Gregory Verret"
       user-mail-address "gregory.verret@gmail.com")
 
-(let* ((init-file-name "init.el")
-         (medivhok:cfg-directory
-          (file-name-as-directory user-emacs-directory))
-         (medivhok:cfg:bootstrap-directory
-          (file-name-as-directory (expand-file-name "+bootstrap" medivhok:cfg-directory)))
-         (medivhok:cfg:exwm-directory
-          (file-name-as-directory (expand-file-name "+exwm" medivhok:cfg-directory)))
-         (medivhok:cfg:ui-directory
-          (file-name-as-directory (expand-file-name "+ui" medivhok:cfg-directory)))
-         (medivhok:cfg:app-directory
-          (file-name-as-directory (expand-file-name "+app" medivhok:cfg-directory)))
-         (medivhok:cfg:ides-directory
-          (file-name-as-directory (expand-file-name "+ides" medivhok:cfg-directory))))
-
-          (load-file (expand-file-name init-file-name medivhok:cfg:bootstrap-directory))
-          (load-file (expand-file-name init-file-name medivhok:cfg:exwm-directory))
-          (load-file (expand-file-name init-file-name medivhok:cfg:ui-directory))
-          (load-file (expand-file-name init-file-name medivhok:cfg:app-directory))
-          (load-file (expand-file-name init-file-name medivhok:cfg:ides-directory)))
+(let* ((medivhok:cfg-directory
+        (file-name-as-directory user-emacs-directory))
+       (medivhok:cfg:bootstrap-directory
+        (file-name-as-directory (expand-file-name "+bootstrap" medivhok:cfg-directory)))
+       (medivhok:cfg:exwm-directory
+        (file-name-as-directory (expand-file-name "+exwm" medivhok:cfg-directory)))
+       (medivhok:cfg:ui-directory
+        (file-name-as-directory (expand-file-name "+ui" medivhok:cfg-directory)))
+       (medivhok:cfg:app-directory
+        (file-name-as-directory (expand-file-name "+app" medivhok:cfg-directory)))
+       (medivhok:cfg:ides-directory
+        (file-name-as-directory (expand-file-name "+ides" medivhok:cfg-directory)))
+       (init-file-name "init.el"))
+        (load-file (expand-file-name init-file-name medivhok:cfg:bootstrap-directory))
+        (load-file (expand-file-name init-file-name medivhok:cfg:exwm-directory))
+        (load-file (expand-file-name init-file-name medivhok:cfg:ui-directory))
+        (load-file (expand-file-name init-file-name medivhok:cfg:app-directory))
+        (load-file (expand-file-name init-file-name medivhok:cfg:ides-directory)))
+)
 ;; init.el ends here.
